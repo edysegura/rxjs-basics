1b1932168f52b53ae4e1aa0ae1cc2d54012dac72
Edy Segura <edysegura@gmail.com>
2019-11-14 23:08:29 -0300

    Add mergeMap operator example

---
+++ merge-map/index.js
@@ -0,0 +1,35 @@
+import { fromEvent } from 'rxjs';
+import { mergeMap, map } from 'rxjs/operators';
+
+const firstField = document.getElementById('first')
+const secondField = document.getElementById('second')
+const output = document.querySelector('output')
+
+const getFieldValue = () =>
+  map(event => event.target.value)
+
+const mergeValues = firstFieldValue => {
+  const concatenateValues = secondFieldValue =>
+    `${ firstFieldValue } ${ secondFieldValue }`
+
+  return secondField$
+    .pipe(
+      getFieldValue(),
+      map(concatenateValues)
+    )
+}
+
+const showInputValue = value => {
+  console.log('Passed here!')
+  output.textContent = value
+}
+
+const firstField$ = fromEvent(firstField, 'input')
+const secondField$ = fromEvent(secondField, 'input')
+
+firstField$
+  .pipe(
+    getFieldValue(),
+    mergeMap(mergeValues)
+  )
+  .subscribe(showInputValue)
