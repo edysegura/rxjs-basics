9175543e1fff5a62340a1466f0383a68909337fa
Edy Segura <edysegura@gmail.com>
2019-11-18 08:47:09 -0300

    Switch map cleanup

--- switch-map/index.js
+++ switch-map/index.js
@@ -1,15 +1,10 @@
 import { fromEvent } from 'rxjs'
-import { scan } from 'rxjs/operators'
 
 const button = document.querySelector('button')
 const output = document.querySelector('output')
 
-const counter = () =>
-  scan(count => count + 1, 0)
-
-const printer = count =>
-  output.textContent = `Clicked ${ count } times`
+const printer = event =>
+  output.textContent = event.target.value
 
 fromEvent(button, 'click')
-  .pipe(counter())
   .subscribe(printer)
