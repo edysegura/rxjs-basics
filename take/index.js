d5abeef173de347d499ff270f2003089d5029d68
Edy Segura <edysegura@gmail.com>
2019-11-27 09:44:48 -0300

    Refactoring take example

--- take/index.js
+++ take/index.js
@@ -1,12 +1,13 @@
 import { interval } from 'rxjs'
 import { take } from 'rxjs/operators'
 
-const source$ = interval(1000)
-  .pipe(take(3))
-
 const showValue = value => {
   const output = document.querySelector('output')
   output.textContent = value
 }
 
-source$.subscribe(showValue)
+const source$ = interval(1000)
+
+source$
+  .pipe(take(6))
+  .subscribe(showValue)
